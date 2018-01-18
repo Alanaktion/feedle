fe8db42883804c2b8c9331aa1ef8c7c16d5fd26b
Alan Hardman <alanaktion@gmail.com>
2018-01-18 11:01:10 -0700

    Minor fixes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('feed:update')
+        $schedule->command('update-feeds')
                  ->everyFifteenMinutes();
     }
 
