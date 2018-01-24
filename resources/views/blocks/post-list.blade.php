8a7d5a392f2684d57dd1c9fbad15f2bb6791eb0f
Alan Hardman <alanaktion@gmail.com>
2018-01-23 21:10:01 -0700

    Adding icon for No unread posts.

--- resources/views/blocks/post-list.blade.php
+++ resources/views/blocks/post-list.blade.php
@@ -8,6 +8,10 @@
     </a>
 @empty
     <div class="list-group-item disabled text-center">
+        <svg class="octicon octicon-check" viewBox="0 0 12 16" version="1.1" aria-hidden="true">
+            <path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5z"></path>
+        </svg>
+        <br>
         No unread posts.
     </div>
 @endforelse
