7d61bed2c299041e2f4d650af07e7e677e4ccdc1
Alan Hardman <alanaktion@gmail.com>
2018-01-26 13:47:33 -0700

    Fixing ProxyController class name

--- app/Http/Controllers/ProxyController.php
+++ app/Http/Controllers/ProxyController.php
@@ -2,20 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
-
-class IndexController extends Controller
-{
-    /**
-     * Show a favicon for a domain, caching it when possible.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function favicon(string $host)
-    {
-        // TODO: find and load $host's favicon
-        // TODO: determine filename and write file
-        // Storage::disk('public')->put($filename, $data);
-    }
+class ProxyController extends Controller {
+	/**
+	 * Show a favicon for a domain, caching it when possible.
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function favicon(string $host) {
+		// TODO: find and load $host's favicon
+		// TODO: determine filename and write file
+		// Storage::disk('public')->put($filename, $data);
+	}
 }
