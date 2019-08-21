cb1435f4ccd403afefb793ce2075f03b0741e0aa
Alan Hardman <alanaktion@gmail.com>
2019-08-21 13:15:03 -0600

    Adding CSS-based tooltip system

--- resources/js/components/AppReader.vue
+++ resources/js/components/AppReader.vue
@@ -18,8 +18,8 @@
                         Feeds
                     </button>
                 </div>
-                <button type="button" class="btn btn-secondary" title="Add Feed"
-                    @click="addFeed">
+                <button type="button" class="btn btn-secondary" aria-label="Add Feed"
+                    data-tooltip="Add Feed" @click="addFeed">
                     <svg class="octicon octicon-plus" viewBox="0 0 12 16" version="1.1" aria-hidden="true">
                         <path fill-rule="evenodd" d="M12 9H7v5H5V9H0V7h5V2h2v5h5z"></path>
                     </svg>
